9e95271c697e509124a09d84310764d16502031a
Rey <rey.fidel@jointheleague.org>
2019-05-04 13:29:43 -0700

    secret message changes

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -17,16 +17,26 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main (String args[] ) {
 	
-	// 1. Set a password in a String variable
+		// 1. Set a password in a String variable
+	String password = null;
 	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+		// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+		password = JOptionPane.showInputDialog(null, "Operator 1.  Set the password");
 
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
+		String guess = JOptionPane.showInputDialog(null, "Operator 2.  Guess the password");
+		//    if they can guess the passcode
+		if ( guess.equals(password)) {
 
-	// 4. If their guess matches the password, show them the secret message
+			// 4. If their guess matches the password, show them the secret message
+			JOptionPane.showMessageDialog(null, password);
+		} else {
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+			// 5. If the password does not match, pop-up "INTRUDER!!"
+		
+			JOptionPane.showMessageDialog(null,"INTRUDER" );
+		}
+	}
 }
