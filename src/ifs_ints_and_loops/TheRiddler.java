6498baaab39fac6a198e6f0176280047a5595c11
Rey <rey.fidel@jointheleague.org>
2019-05-08 21:21:02 -0700

    Update TheRiddler.java

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,4 +1,7 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -9,17 +12,41 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		score += TheRiddler.AskRiddle( "what is white and goes up", "retarded snowflake");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one''
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+		// 5. Otherwise, say "wrong" and tell them the answer		
+		
 
 		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+		score += AskRiddle( "what black and white, and red all over", "book");
 		
+		// 2. Make a pop up to show the score.	
+		JOptionPane.showMessageDialog(null, "Score = " +  score);
+	}
+	
+	private static int AskRiddle(String riddle, String answer)
+	{
+		int score = 0;
+		String guess = JOptionPane.showInputDialog(riddle);
+		if (guess == null )
+		{
+			// do nothing
+		}
+		else if ( guess.equalsIgnoreCase(answer) )
+		{
+			score = 1;
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Wrong.  " + answer);
+		}
+		return score;
 	}
 }
 
